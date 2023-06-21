c4bf6b09c1dff3d48529112c470c1a6d612ad899
Joseph.K <joseph.kamwana@student.moringashool.com>
2023-06-21 22:39:23 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide = (parameter1, parameter2) => 
+ parameter1 / parameter2;
+ divide(2000, 100);
+
+let x =2;
+ const square = x = x*x;
+ 
+
+let a = 3;
+let b = 4;
+const add = (a,b) => (a+b);
+
+    
+
+
+
+
+
+
